ee81ef3368754d660d3ae638e04149aa62348320
0xirvan <darknet1255@gmail.com>
2025-02-09 11:55:45 +0700

    chore: adjust padding in introduction component section

--- components/introduction.tsx
+++ components/introduction.tsx
@@ -3,7 +3,7 @@ import HeadingText from "@/components/heading-text";
 
 function Introduction() {
   return (
-    <section className="space-y-12 px-4 md:px-0">
+    <section className="space-y-12 px-2 md:px-0">
       <div>
         <h1 className="text-xl md:text-2xl mb-4">Sup, i'm Irvan pramana 👋</h1>
         <p>Full-stack developer who loves open-source</p>
