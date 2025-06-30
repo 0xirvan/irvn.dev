882de3d1f3047e25c2c772b79a84ea48040b6d39
irvan pramana <darknet1255@gmail.com>
2025-07-01 05:08:01 +0700

    feat: add CV for Irvan Pramana and update resume URL in profile

--- src/components/hero/profile.ts
+++ src/components/hero/profile.ts
@@ -2,7 +2,7 @@ export const profile = {
   name: "Irvan Pramana",
   description: `Sup, i'm Irvan pramana backend developer who loves open-source. i enjoy building APIs and systems using Java and Golang, and i like learning new technologies. My favorite stack includes Spring Boot for Java development and building high-performance services with Go. currently, exploring system design and distributed systems.`,
   profileImage: "https://avatars.githubusercontent.com/u/52144823",
-  resumeUrl: "/resume.pdf",
+  resumeUrl: "/CV Irvan pramana.pdf",
   socials: {
     instagram: "https://instagram.com/0xirvan",
     linkedin: "https://linkedin.com/in/0xirvan",
